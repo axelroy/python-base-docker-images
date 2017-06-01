cf65c6a06aa8df4b3755cb45f69989ef06e7538a
axelroy <royaxe@gmail.com>
2017-06-01 16:25:54 +0200

    First simple version without entries and output, to be done in the next version by Docker Volumes in text files.

--- python-mip/scripts/docker-entrypoint.sh
+++ python-mip/scripts/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "compute" ]; then
+if [ "$1" = "train" ]; then
 	echo "Running python-histogram computation..."
-	python /main.py
+	python /main.py train
+fi
+
+if [ "$1" = "test" ]; then
+	echo "Running python-histogram computation..."
+	python /main.py test
 fi
