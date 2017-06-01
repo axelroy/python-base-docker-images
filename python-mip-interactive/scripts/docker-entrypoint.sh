8e155bd974b381dd6c562cbf90aff2b8c41c503d
axelroy <royaxe@gmail.com>
2017-06-01 16:25:24 +0200

    First simple version without entries and output, to be done in the next version by Docker Volumes in text files.

--- python-mip-interactive/scripts/docker-entrypoint.sh
+++ python-mip-interactive/scripts/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "train" ]; then
 	echo "Running training..."
-	python /main.py
+	python /main.py train
 fi
 
 if [ "$1" = "test" ]; then
 	echo "Running test..."
-	python /main.py
+	python /main.py test
 fi
--- python-mip-interactive/scripts/main.py
+++ python-mip-interactive/scripts/main.py
@@ -1,5 +1,32 @@
-def main():
-    print("Hello")
+import sys
+import json
+from collections import Counter
+
+array = '{"values": [12, 13, 14, 14]}'
+
+def train():
+    """Does de average of the given data"""
+    print("Train")
+    data  = json.loads(array)
+    numbers = data['values']
+    numbers_sum = sum(numbers) / len(numbers)
+    print(numbers_sum)
+
+def test():
+    """Does the modus of the datas given (for the tests)"""
+    print("Test")
+    data  = json.loads(array)
+    numbers = data['values']
+    data = Counter(numbers)
+    mode = data.most_common(1)
+    print("Mode : ", mode)
+
+def main(argv):
+    """Documentation can be placed here, but not in the __name__ function"""
+    if argv[0] == "train":
+        train()
+    elif argv[0] == "test":
+        test()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
