caeac55cca8722a4fa589a489530aad057e33062
axelroy <royaxe@gmail.com>
2017-06-08 15:59:02 +0200

    Updating the README for the first version

--- python-mip-interactive/scripts/main.py
+++ python-mip-interactive/scripts/main.py
@@ -2,23 +2,39 @@ import sys
 import json
 from collections import Counter
 
-array = '{"values": [12, 13, 14, 14]}'
+input_file = '/docker-volume/data.json'
+output_file = '/docker-volume/out.json'
 
 def train():
     """Does de average of the given data"""
     print("Train")
-    data  = json.loads(array)
+    # data  = json.loads(array)
+    with open(input_file) as data_file:
+        data = json.load(data_file)
+
+    print("read from file")
+
     numbers = data['values']
     numbers_sum = sum(numbers) / len(numbers)
+
+    with open(output_file, 'w') as outfile:
+        outfile.write(str(numbers_sum))
+
     print(numbers_sum)
 
 def test():
     """Does the modus of the datas given (for the tests)"""
     print("Test")
-    data  = json.loads(array)
+    with open('/docker-volume/data.json') as data_file:
+        data = json.load(data_file)
+
+    print("read from file")
     numbers = data['values']
     data = Counter(numbers)
     mode = data.most_common(1)
+
+    with open(output_file, 'w') as outfile:
+        outfile.write(str(mode))
     print("Mode : ", mode)
 
 def main(argv):
